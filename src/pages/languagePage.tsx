14241e990d9aaf1e0384c275771a8a4346546818
Hong-Shao <joee0511@gmail.com>
2022-03-31 15:38:09 +0800

    feat: :sparkles: construct default content of each page

---
+++ src/pages/languagePage.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+
+import '../utils/font-format.scss';
+
+export const languagePage = (): JSX.Element => {
+  return (
+    <Box>
+
+    </Box>
+  );
+}
