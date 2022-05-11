cf5b740462c11e97a51f937638e814eaa3b59a0b
Hong-Shao <joee0511@gmail.com>
2022-05-11 23:59:42 +0800

    fix: :adhesive_bandage: remove unused introduced variable

--- backend/src/main/java/com/backend/server/services/UserService.java
+++ backend/src/main/java/com/backend/server/services/UserService.java
@@ -6,9 +6,7 @@ import java.util.Optional;
 import com.backend.server.dto.NewUserDto;
 import com.backend.server.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
 
@@ -46,7 +44,7 @@ public class UserService {
     }
 
     public void createNewUser(NewUserDto newUserDto) {
-        User user = new User(
+        new User(
                 newUserDto.getUsername(),
                 passwordEncoder.encode(newUserDto.getPassword()),
                 newUserDto.getEmail(),
